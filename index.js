0685e77cc79c6f5b515e26b45095ae0fd60b299f
Andrew Lively <andrew.lively2@gmail.com>
2017-09-12 12:00:06 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const { send } = require('micro');
+const query = require('micro-query');
+
+function generateRandomNumber(min = 1, max = 5) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  if (min > max) {
+    max = min + 1;
+  }
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+module.exports = (req, res) => {
+  const { min, max } = query(req);
+  const number = generateRandomNumber(min, max);
+
+  send(res, 200, { number });
+};
